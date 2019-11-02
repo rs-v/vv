45eb07e89e2b71baa0176114eba3f72d1f5b25d6
13863049525 <w42023@outlook.com>
2019-11-02 15:09:12 +0800

    (>_<)auto

---
+++ sw.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+importScripts('sw-toolbox.js');
+
+toolbox.precache(["index.html","style/style.css"]);
+
+toolbox.router.get('/images/*', toolbox.cacheFirst);
+
+toolbox.router.get('/*', toolbox.cacheFirst, {
+  networkTimeoutSeconds: 5
+});
